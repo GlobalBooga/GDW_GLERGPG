46204bdc1fb77b28af51658d79e7bece2cda5393
GlobalBooga <alex_anasta@hotmail.ca>
2023-11-07 15:05:58 -0500

    menu start

---
+++ Assets/Scripts/Menus/EmissiveButton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EmissiveButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler
+{
+    [SerializeField] Sprite normalSprite;
+    [SerializeField] Sprite hoverSprite;
+    [SerializeField] float clickScale = 1.1f;
+
+    Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one * clickScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!hoverSprite) return;
+        image.sprite = hoverSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one;
+
+        if (!normalSprite) return;
+        image.sprite = normalSprite;
+    }
+}
