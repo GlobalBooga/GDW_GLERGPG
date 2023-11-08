fdc0e0acfd18380d31185bc042d2372dbc55c331
IzzyDev <kishoreishaan@gmail.com>
2023-11-07 19:48:53 -0500

    Pre-cinemachine
    In case something goes wrong

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -5,11 +5,13 @@ public class PlayerManager : MonoBehaviour
 {
     private InputManager inputManager;
     private PlayerLocomotion playerLocomotion;
+    private CameraManager cameraManager;
     
     private void Start()
     {
         inputManager = GetComponent<InputManager>();
         playerLocomotion = GetComponent<PlayerLocomotion>();
+        cameraManager = FindObjectOfType<CameraManager>();
     }
 
     private void Update()
@@ -20,5 +22,10 @@ public class PlayerManager : MonoBehaviour
     private void FixedUpdate()
     {
         playerLocomotion.HandleAllMovement();
+    }
+
+    private void LateUpdate()
+    {
+        cameraManager.FollowTarget();
     }
 }
