85fae61131fb21946f25614ed80c1265aac2c5eb
Donovan Mills <donovan.mills@ontariotechu.net>
2023-11-09 21:36:26 -0500

    Put sample code
    Wrote the code sample to substitute the fmod event name in once they have been implemented.

---
+++ Assets/Scripts/Player/FindVolume.cs
@@ -0,0 +1,24 @@
+using FMOD;
+using FMOD.Studio;
+using FMODUnity;
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindVolume : MonoBehaviour
+{
+    private float volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RESULT "EventInstance".getVolume(out float volume);
+    }
+}
