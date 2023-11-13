441d2319a8bd1f7e31d794f9ebac6d6a90f76772
GlobalBooga <alex_anasta@hotmail.ca>
2023-11-13 10:11:00 -0500

    settings

---
+++ Assets/Scripts/Menus/MouseSensSlider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class MouseSensSlider : MonoBehaviour
+{
+    Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener((value) => { SaveValue(value); });
+        slider.value = PlayerPrefs.GetFloat("Sensitivity", 1);
+    }
+
+    public void SaveValue(float value)
+    {
+        PlayerPrefs.SetFloat("Sensitivity", value);
+    }
+}
---
+++ Assets/Scripts/Menus/VolumeSlider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener((value) => { SaveValue(value); });
+        slider.value = PlayerPrefs.GetFloat("Volume", 1);
+    }
+
+    public void SaveValue(float value)
+    {
+        PlayerPrefs.SetFloat("Volume", value);
+    }
+}
