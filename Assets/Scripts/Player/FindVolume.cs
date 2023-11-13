e016f7338d99c74c11905c215e6929656a7a2eb6
Donovan Mills <donovan.mills@ontariotechu.net>
2023-11-13 12:52:44 -0500

    Push 2

--- Assets/Scripts/Player/FindVolume.cs
+++ Assets/Scripts/Player/FindVolume.cs
@@ -1,16 +1,10 @@
 using FMOD;
-<<<<<<< Updated upstream
 using FMOD.Studio;
 using FMODUnity;
-=======
->>>>>>> Stashed changes
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
-=======
-
 public class FindVolume : MonoBehaviour
 {
     [HideInInspector] public float playerVolume;
@@ -26,24 +20,5 @@ public class FindVolume : MonoBehaviour
     void Update()
     {
         playerChannel.getAudibility(out playerVolume);
-    }
-}
-
->>>>>>> Stashed changes
-
-public class FindVolume : MonoBehaviour
-{
-    private float volume;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //RESULT "EventInstance".getVolume(out float volume);
     }
 }
