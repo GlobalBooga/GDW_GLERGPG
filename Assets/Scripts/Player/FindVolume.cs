bc6cad3cd35d3a82e4cc5191e9fde546f0af5cef
Donovan Mills <donovan.mills@ontariotechu.net>
2023-11-13 12:09:22 -0500

    Update FindVolume.cs

--- Assets/Scripts/Player/FindVolume.cs
+++ Assets/Scripts/Player/FindVolume.cs
@@ -1,10 +1,35 @@
 using FMOD;
+<<<<<<< Updated upstream
 using FMOD.Studio;
 using FMODUnity;
+=======
+>>>>>>> Stashed changes
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
+=======
+
+public class FindVolume : MonoBehaviour
+{
+    [HideInInspector] public float playerVolume;
+    public Channel playerChannel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerChannel = GetComponent<Channel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerChannel.getAudibility(out playerVolume);
+    }
+}
+
+>>>>>>> Stashed changes
 
 public class FindVolume : MonoBehaviour
 {
